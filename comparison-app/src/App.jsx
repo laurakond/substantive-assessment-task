d9a43ceff2f17912c3a8163fa1859464b8211894
laurakond <laura.kondrataite@gmail.com>
2025-04-04 13:07:47 +0100

    initial commit & component set up

---
+++ comparison-app/src/App.jsx
@@ -0,0 +1,18 @@
+// Import the necessary modules and components
+import React from 'react'
+import './App.css'
+import Comparison from './components/comparison'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Comparison />
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ comparison-app/src/components/Comparison.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Comparison = () => {
+  return <div>comparison</div>;
+};
+
+export default Comparison;
