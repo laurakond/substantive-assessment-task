a0719a18ace6427a78ccb64f5794f4f7c6679bd1
laurakond <laura.kondrataite@gmail.com>
2025-04-10 10:48:01 +0100

    add code for setting up chart legend

--- comparison-app/src/components/BarChart.jsx
+++ comparison-app/src/components/BarChart.jsx
@@ -21,24 +21,31 @@ ChartJS.register(
 
 const BarChart = ({ payment, benchmark }) => {
   const data = {
-    labels: ["Total"],
+    labels: ["Payment (€)", "Benchmark (€)"],
     datasets: [
       {
-        label: "Payment (€)",
-        data: [payment],
-        backgroundColor: "rgba(54, 162, 235, 0.5)",
-      },
-      {
-        label: "Benchmark (€)",
-        data: [benchmark],
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
+        label: [payment, benchmark],
+        data: [payment, benchmark],
+        backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(255, 159, 64, 0.2)"],
       },
     ],
   };
   const options = {
     responsive: true,
     plugins: {
-      legend: { position: "top" },
+      legend: {
+        position: "top",
+        labels: {
+          generateLabels: (chart) => {
+            console.log("chart: ", chart);
+            return chart.data.labels.map((label, index) => ({
+              text: label,
+              strokeStyle: chart.data.datasets[0].backgroundColor[index],
+              fillStyle: chart.data.datasets[0].backgroundColor[index],
+            }));
+          },
+        },
+      },
       title: { display: true, text: "Total payment vs Total benchmark" },
     },
   };
