cc50668cf8378bddbbe048326f44afbd012ec0e5
laurakond <laura.kondrataite@gmail.com>
2025-04-12 19:28:17 +0100

    format code

--- comparison-app/src/components/DisplayCalculations.jsx
+++ comparison-app/src/components/DisplayCalculations.jsx
@@ -11,21 +11,36 @@ const DisplayCalculations = ({
     <div className="py-4">
       {/** Displays the total payment and benchmark values in Euro for the
        * selected year and provider name */}
-      <h4>Total Payment: €{totalSumPayment}</h4>
-      <h4>Total Benchmark: €{totalSumBenchmark}</h4>
-      <h4>
-        Difference: €{totalSumPayment - totalSumBenchmark}
+      <span style={{ display: "block" }}>
+        <strong>Total Payment: </strong>€{totalSumPayment}
+      </span>
+      <span style={{ display: "block" }}>
+        <strong>Total Benchmark: </strong>€{totalSumBenchmark}
+      </span>
+      <span style={{ display: "block" }}>
+        <strong>Difference: </strong>€{totalSumPayment - totalSumBenchmark}
         {totalSumPayment - totalSumBenchmark < 0 ? (
-          <span style={{ color: "red" }}> (Under benchmark)</span>
-        ) : (
-          <span style={{ color: "green" }}> (Over benchmark)</span>
-        )}
-      </h4>
+          <span style={{ color: "red" }}>
+            {" "}
+            <strong>(Under benchmark)</strong>
+          </span>
+        ) : totalSumPayment - totalSumBenchmark > 0 ? (
+          <span style={{ color: "green" }}>
+            {" "}
+            <strong>(Over benchmark)</strong>
+          </span>
+        ) : null}
+      </span>
+
       {selectedProviderName !== "Company Name" &&
         selectedYear !== "Select Year" &&
         noDataAvailable && (
           <p>
-            No data available for {selectedProviderName} {selectedYear}.
+            No data available for{" "}
+            <strong>
+              {selectedProviderName} {selectedYear}
+            </strong>
+            .
           </p>
         )}
     </div>
