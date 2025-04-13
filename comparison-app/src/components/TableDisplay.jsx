bc74ae1c30e3a2fa7bc46533ebc91a82d12b380d
laurakond <laura.kondrataite@gmail.com>
2025-04-13 14:36:18 +0100

    add no data display code

--- comparison-app/src/components/TableDisplay.jsx
+++ comparison-app/src/components/TableDisplay.jsx
@@ -4,21 +4,15 @@ import React from "react";
 const TableDisplay = ({
   totalSumPayment,
   totalSumBenchmark,
-
+  noDataAvailable,
   percentageDifference,
   formattedDifference,
+  selectedProviderName,
+  selectedYear,
 }) => {
-  {
-    percentageDifference < 0 ? (
-      <p>{percentageDifference}% under benchmark</p>
-    ) : (
-      <p>{percentageDifference}% over benchmark</p>
-    );
-  }
-
   return (
     <>
-      <div>
+      <div className="pt-4">
         <Table striped bordered hover>
           <thead>
             <tr>
@@ -48,51 +42,18 @@ const TableDisplay = ({
           </tbody>
         </Table>
       </div>
-      {/* <div>
-        <h5>Breakdown of figures for all companies for {selectedYear} </h5>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Company Name</th>
-              <th>Total Payment</th>
-              <th>Total Benchmark</th>
-              <th>Difference</th>
-              <th>Difference in %</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{selectedProviderName}</td>
-              <td>{totalSumPayment}</td>
-              <td>{totalSumBenchmark}</td>
-              <td>{formattedDifference}</td>
-              <td>
-                {percentageDifference < 0 ? (
-                  <span style={{ color: "red" }}>{percentageDifference}%</span>
-                ) : percentageDifference > 0 ? (
-                  <span style={{ color: "green" }}>
-                    {percentageDifference}%
-                  </span>
-                ) : (
-                  <span>0.00%</span>
-                )}
-              </td>
-            </tr>
-            <tr>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <td>Larry the Bird</td>
-              <td>Larry the Bird</td>
-              <td>@twitter</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </Table>
-      </div> */}
+      {selectedProviderName !== "Company Name" &&
+        selectedYear !== "Select Year" &&
+        noDataAvailable && (
+          <div>
+            <p className="mt-4" style={{ color: "red" }}>
+              No data available for{" "}
+              <strong>
+                {selectedProviderName} {selectedYear}.
+              </strong>
+            </p>
+          </div>
+        )}
     </>
   );
 };
